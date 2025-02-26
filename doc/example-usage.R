9f6a968753fbeb57fc28d4c4bcb8ca45cb052f8b
Connor Quiroz <quir1869@uw.edu>
2025-02-26 12:27:41 -0800

    updated gitignore

---
+++ doc/example-usage.R
@@ -0,0 +1,16 @@
+## ----include = FALSE----------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup--------------------------------------------------------------------
+## load pets package
+library(pets)
+
+## if you love cats
+cats(TRUE)
+
+## if you're not a big fan of cats
+cats(FALSE)
+
